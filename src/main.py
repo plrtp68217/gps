12ee0c8a3085d1151c37fd05225a722b6d9f4418
plrtp <plrtp@mail.ru>
2024-10-01 21:23:46 +0300

    исправлен баг неправльного сложения дробей

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from generators import generate_exp_time, generate_exp_len
+from fractions import Fraction
 
-number_of_threads = 3 # число потоков
+number_of_threads = 2 # число потоков
 
-thread_priorities = [1, 2, 3] # приоритеты каждого потока
+thread_priorities = [1, 2] # приоритеты каждого потока
 
 active_threads = {} # активные потоки (обрабатывающие пакеты) (0 - не активен, 1 - активен)
 
@@ -12,20 +13,30 @@ packets_time = {} # время прихода пакетов для каждог
 
 packets_endtime = {} # время обработки пакетов для каждого потока
 
-time_tracker = {} # какие прибывшие пакеты были переведены в обработку
-
-packets_handling = {} # процесс обработки пакетов 
+packets_handling = {} # процесс обработки пакетов
 
 packets_len = {} # длина поступивших пакетов для каждого потока
+
+time_tracker = {} # какие прибывшие пакеты были переведены в обработку
 
 processing = True # индикатор, отображающий работу планировщика (при обработке всех пакетов перевести в false)
 
 cycle_counter = 0 # счетчик циклов работы панировщика (каждый пройденный цикл - единица времени работы панировщика)
 
+packets_time = {
+    1: [1, 2, 3, 11],
+    2: [0, 5, 9]
+}
+
+packets_len = {
+    1: [1, 1, 2, 2],
+    2: [3, 2, 2]
+}
+
 for thread in range(number_of_threads):
-    packets_time[thread + 1] = generate_exp_time()
+    # packets_time[thread + 1] = generate_exp_time()
     packets_endtime[thread + 1] = []
-    packets_len[thread + 1] = generate_exp_len(packets_time[thread + 1])
+    # packets_len[thread + 1] = generate_exp_len(packets_time[thread + 1])
     time_tracker[thread + 1] = [0]
     packets_handling[thread + 1] = []
     active_threads[thread + 1] = 0
@@ -53,11 +64,11 @@ while processing:
     # обработка пакетов активных потоков
     for processing_thread in range(len(active_threads)):
         if active_threads[processing_thread + 1] == 1:
-            packets_handling[processing_thread + 1][2] += thread_priorities[processing_thread] / summary # обрабатываем пакет на величину загрузки (приоритет потока / summary)
+            packets_handling[processing_thread + 1][2] += Fraction(thread_priorities[processing_thread], summary) # обрабатываем пакет на величину текущей загрузки (приоритет потока / summary)
             if packets_handling[processing_thread + 1][2] >= packets_handling[processing_thread + 1][1]: #если пакет обработан
                 packets_handling[processing_thread + 1].clear()
                 active_threads[processing_thread + 1] = 0 # переводим поток в состояние неактивного
-                packets_endtime[processing_thread + 1].append(cycle_counter)
+                packets_endtime[processing_thread + 1].append(cycle_counter + 1)
                 if len(time_tracker[processing_thread + 1]) < len(packets_time[processing_thread + 1]):
                     time_tracker[processing_thread + 1].append(time_tracker[processing_thread + 1][-1] + 1)
                 elif len(time_tracker[processing_thread + 1]) == len(packets_time[processing_thread + 1]):
@@ -70,14 +81,13 @@ while processing:
         end_processing = end_processing and completed_threads[second_thread]
 
     if end_processing: # если все пакеты обработаны
-        break # завершить работу планировщика
+        processing = False # завершить работу планировщика
 
     # переход на следующую временную единицу
     cycle_counter += 1
 
-
-
-
+print(packets_time)
+print(packets_endtime)
 
 
 
