65e2eefcca05361f73cdbbc34be55e42d2441fdb
plrtp <plrtp@mail.ru>
2024-10-01 16:47:04 +0300

    добавлены переменные для хранения требуемых значений алгоритма

--- src/generators.py
+++ src/generators.py
@@ -3,10 +3,10 @@ import random
 def generate_exp_time() -> list:
     ''' Формирует случайное время прихода пакетов'''
     rate = 0.5
-    exp_time = set([round(10 * random.expovariate(rate)) for i in range(10)])
+    exp_time = set([round(10 * random.expovariate(rate)) for _ in range(10)])
     return sorted(exp_time)
 
-def generate_exp_len(packets: dict) -> list:
+def generate_exp_len(packets: list) -> list:
     ''' Формирует случайные длины для пакетов'''
-    exp_len = [round(10 * random.random()) for i in packets]
+    exp_len = [round(10 * random.random()) + 1 for _ in packets]
     return exp_len
--- src/main.py
+++ src/main.py
@@ -2,14 +2,37 @@ from generators import generate_exp_time, generate_exp_len
 
 number_of_threads = 3 # число потоков
 
+thread_priorities = [1, 2, 3] # приоритеты каждого потока
+
 packets_time = {} # время прихода пакетов для каждого потока
+
+packets_endtime = {} # время обработки пакетов для каждого потока
 
 packets_len = {} # длина поступивших пакетов для каждого потока
 
+processing = True # индикатор, отображающий работу планировщика (при обработке всех пакетов перевести в false)
+
+cycle_counter = 0 # счетчик циклов работы панировщика (каждый пройденный цикл - единица времени работы панировщика)
+
 for thread in range(number_of_threads):
     packets_time[thread + 1] = generate_exp_time()
+    packets_len[thread + 1] = generate_exp_len(packets_time[thread + 1])
 
- 
-print(generate_exp_len(packets_time))
+
+# while processing:
+#     for 
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
